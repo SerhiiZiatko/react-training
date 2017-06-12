8b87caa2ed9e90ebda766f8e9e4ee9c2369d9f8f
Yuriy Cherenkov <cherenkov.yuriy@nixsolutions.com>
2017-06-12 19:20:34 +0300

    init project

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>React redux template</h2>
         </div>
       </div>
     );
